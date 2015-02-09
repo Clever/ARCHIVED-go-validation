a0b1f2b449e858bbf6c7ff383967647feca89a2a
Mike Graf <mike.graf@clever.com>
2015-02-09 15:15:04 -0800

    Covers most cases of required validator

--- required.go
+++ required.go
@@ -1,4 +1,4 @@
-package validator
+package validation
 
 import (
 	"fmt"
@@ -6,35 +6,73 @@ import (
 	"strings"
 )
 
+type Option uint8
+
+const (
+	// Allows empty string, array or map. Default is false
+	AllowEmptyString Option = iota
+	AllowEmptyArray
+	AllowEmptyMap
+	AllowEmptySlice
+)
+
 // Required Validator is very simple
-type RequiredValidator struct{}
+type Required struct {
+	opts map[Option]bool
+}
+
+func (r *Required) SetOpts(opts map[Option]bool) {
+	r.opts = opts
+}
 
 // IsValid determines if the given value satisfies a "Required" rule.
-func (r *RequiredValidator) IsValid(value interface{}) (bool, []error) {
-
+func (r *Required) IsValid(value interface{}) (bool, []error) {
 	if value == nil {
-		return false, singleErrorSlice("nil is not a valid value for RequiredValidator")
+		return false, singleErrorSlice("nil is not a valid value for Required")
 	}
 
-	switch reflect.TypeOf(value).Kind() {
+	kind := reflect.TypeOf(value).Kind()
+	switch kind {
+	case reflect.Struct, reflect.Bool, reflect.Int, reflect.Float32, reflect.Float64:
+		return true, nil
 	case reflect.String:
 		return r.stringIsValid(value.(string))
-
-	default:
-		panic("unhandle value type")
-		return false, singleErrorSlice("Cannot handle value(%v)", value)
+	case reflect.Map, reflect.Array, reflect.Slice:
+		var override bool
+		switch kind {
+		case reflect.Map:
+			override = r.opts[AllowEmptyMap]
+		case reflect.Array:
+			override = r.opts[AllowEmptyArray]
+		case reflect.Slice:
+			override = r.opts[AllowEmptySlice]
+		}
+		return r.collectionLengthOK(kind.String(), reflect.ValueOf(value).Len(), override)
 	}
+
+	// Default case
+	return false, singleErrorSlice("Cannot handle value(%v)", value)
 }
 
 // stringIsValid ensures a string is valid (Not empty after trimming)
-func (r *RequiredValidator) stringIsValid(value string) (bool, []error) {
-	if strings.TrimSpace(value) != "" {
+func (r *Required) stringIsValid(value string) (bool, []error) {
+	if strings.TrimSpace(value) != "" || r.opts[AllowEmptyString] {
 		return true, nil
 	} else {
-		return false, singleErrorSlice("String value(%v) doesnt not satisfy RequiredValidator", value)
+		return false, singleErrorSlice("String value(%v) doesnt not satisfy Required", value)
 	}
 }
 
+// collectionLengthOK returns true if it contains a value (ie, nonempty). False if empty, unless the option is set.
+func (r *Required) collectionLengthOK(colType string, length int, override bool) (bool, []error) {
+	if length > 0 || override {
+		return true, nil
+	} else {
+		return false, singleErrorSlice("%s length(%d) does not satisfy Required(override=%t)", colType, length, override)
+	}
+}
+
+// singleErrorSlice is a helper to match the interface []error
 func singleErrorSlice(e string, args ...interface{}) []error {
 	return append(make([]error, 0), fmt.Errorf(e, args...))
 }
--- required_test.go
+++ required_test.go
@@ -1,13 +1,13 @@
-package validator
+package validation
 
 import (
-	//"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 type testCase struct {
 	Value    interface{}
+	Options  map[Option]bool
 	Expected bool
 }
 
@@ -17,8 +17,15 @@ var requiredCases = [...]testCase{
 		Expected: false,
 	},
 	testCase{
+		Value: "",
+		Options: map[Option]bool{
+			AllowEmptyString: true,
+		},
+		Expected: true,
+	},
+	testCase{
 		Value:    struct{}{},
-		Expected: false,
+		Expected: true,
 	},
 	testCase{
 		Value:    nil,
@@ -27,6 +34,11 @@ var requiredCases = [...]testCase{
 	testCase{
 		Value:    "    ",
 		Expected: false,
+	},
+	testCase{
+		Value:    "    ",
+		Options:  map[Option]bool{AllowEmptyString: true},
+		Expected: true,
 	},
 	testCase{
 		Value:    "testing   ",
@@ -53,8 +65,26 @@ var requiredCases = [...]testCase{
 		Expected: true,
 	},
 	testCase{
+		// Empty Array
 		Value:    [...]interface{}{},
 		Expected: false,
+	},
+	testCase{
+		// Empty Array, with option
+		Value: [...]interface{}{},
+		Options: map[Option]bool{
+			AllowEmptyArray: true,
+		},
+		Expected: true,
+	},
+	testCase{
+		// Array w/ 3 empty structs
+		Value: [...]interface{}{
+			struct{}{},
+			struct{}{},
+			struct{}{},
+		},
+		Expected: true,
 	},
 	testCase{
 		Value: [...]interface{}{
@@ -63,14 +93,55 @@ var requiredCases = [...]testCase{
 		},
 		Expected: true,
 	},
+	testCase{
+		// Empty Slice
+		Value:    []interface{}{},
+		Expected: false,
+	},
+	testCase{
+		// Empty Slice, w/ option
+		Value: []interface{}{},
+		Options: map[Option]bool{
+			AllowEmptySlice: true,
+		},
+		Expected: true,
+	},
+	testCase{
+		// Full slice
+		Value:    []uint{1, 3, 4, 63434},
+		Expected: true,
+	},
+	testCase{
+		// empty map literal
+		Value:    map[int]bool{},
+		Expected: false,
+	},
+	testCase{
+		// empty map literal, with option
+		Value:    map[int]bool{},
+		Options:  map[Option]bool{AllowEmptyMap: true},
+		Expected: true,
+	},
+	testCase{
+		Value:    map[int]bool{1: true, 2: false},
+		Expected: true,
+	},
 }
 
-func TestRequiredValidator(t *testing.T) {
-	rv := RequiredValidator{}
+func TestRequired(t *testing.T) {
+	rv := Required{}
 
 	for _, c := range requiredCases {
+		rv.SetOpts(c.Options)
 		valid, errors := rv.IsValid(c.Value)
-		assert.Equal(t, c.Expected, valid)
-		assert.Equal(t, 1, len(errors))
+		assert.Equal(
+			t,
+			c.Expected,
+			valid,
+			"rv.IsValid(%#v) w/ options(%v) returned unexpected answer (%v)", c.Value, c.Options, c.Expected,
+		)
+		if !c.Expected {
+			assert.Equal(t, 1, len(errors))
+		}
 	}
 }
